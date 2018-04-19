202d2158a936d0f2cab083d7cc2f4be1754db656
hoanganhlam <hoanglam.bk57@gmail.com>
2018-04-19 16:04:42 +0700

    Update

--- src/app/Album/store/actions.js
+++
@@ -1,2 +0,0 @@
-/* eslint-disable */
-import * as types from './mutation-types'
--- src/app/Album/store/mutation-types.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-export const SELECT_MEDIA = 'SELECT_MEDIA';
-export const SELECT_ONE = 'SELECT_ONE';
-export const REMOVE_MEDIA = 'REMOVE_MEDIA';
-export const GET_ALL_MEDIA = 'GET_ALL_MEDIA';
-export const SUBMIT_ALBUM = 'SUBMIT_ALBUM';
-export const SUBMIT_SUCCESS = 'SUBMIT_SUCCESS';
-export const SUBMIT_FAILURE = 'SUBMIT_FAILURE';
-export const GET_GALLERY = 'GET_GALLERY';
